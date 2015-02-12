5d9c3cc23d663054de21a1737a53418a2f1c2b43
50113001 <ryo.adachi2819@gmail.com>
2015-02-13 07:46:44 +0900

    20150202 submit

---
+++ src/graph/Graph_View.java
@@ -0,0 +1,92 @@
+package graph;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class Graph_View extends Frame implements ActionListener,WindowListener{
+    
+	public Graph_View(){
+		addWindowListener(this);
+		setTitle("Graph");
+		
+		DefaultCategoryDataset data = new DefaultCategoryDataset();
+		
+		data.addValue(4, "", "0~25");
+	    data.addValue(5, "", "26~50");
+	    data.addValue(5, "", "51~75");
+	    data.addValue(6, "", "76~100");
+	    
+		JFreeChart chart = 
+	      ChartFactory.createLineChart("Score Distribution ","score","The Number of people",
+	                                   data,
+	                                   PlotOrientation.VERTICAL,
+	                                   true,
+	                                   false,
+	                                   false);
+
+	    ChartPanel cpanel = new ChartPanel(chart);
+	    add(cpanel, BorderLayout.CENTER);
+	  
+
+
+	}
+	
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+	System.exit(0);	
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
